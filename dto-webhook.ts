e28fccca7477cb7da850691c2b62ed513463dc23
Luchito Fantasìa <luisparadawagner@gmail.com>
2018-12-20 14:47:39 -0300

    Se factura para sumar

--- dto-webhook.ts
+++ dto-webhook.ts
@@ -15,41 +15,41 @@ export const prestacionNiñoSano = {
         conceptId: '410620009',
         term: "consulta de niño sano, recién nacido",
         fsn: "consulta de niño sano, recién nacido (procedimiento)",
-        datosReportables: null
-        // datosReportables: [
-        //     {
-        //         conceptId: '27113001',
-        //         term: 'peso corporal',
-        //         valor: '15'
-        //     },
-        //     {
-        //         conceptId: '12345',
-        //         term: 'prueba borrar',
-        //         valor: '555'
-        //     },
-        //     {
-        //         conceptId: '248338008',
-        //         term: 'percentilo de talla',
-        //         valor: '80'
-        //     },
-        //     {
-        //         conceptId: '363812007',
-        //         term: 'perimetro cefalico',
-        //         valor: '90'
-        //     }
-        // ],
+        // datosReportables: null
+        datosReportables: [
+            {
+                conceptId: '27113001',
+                term: 'peso corporal',
+                valor: '15'
+            },
+            {
+                conceptId: '12345',
+                term: 'prueba borrar',
+                valor: '555'
+            },
+            {
+                conceptId: '248338008',
+                term: 'percentilo de talla',
+                valor: '80'
+            },
+            {
+                conceptId: '363812007',
+                term: 'perimetro cefalico',
+                valor: '90'
+            }
+        ],
     },
     organizacion: {
         nombre: "HOSPITAL PROVINCIAL NEUQUEN - DR. EDUARDO CASTRO RENDON",
         cuie: "Q06391",
         idSips: 205
     },
-    // obraSocial: null,
+    obraSocial: null,
     /* Usar este para los que tienen OS*/
-    obraSocial: {
-        codigoFinanciador: '921001',
-        financiador: 'O.S.P. NEUQUEN'
-    },
+    // obraSocial: {
+    //     codigoFinanciador: '921001',
+    //     financiador: 'O.S.P. NEUQUEN'
+    // },
     profesional: {
         nombre: "MARIA LAURA",
         apellido: "MONTEVERDE",
@@ -73,45 +73,45 @@ export const prestacionOtoemisiones = {
         conceptId: '2091000013100',
         term: "screening de otoemisión acústica neonatal (procedimiento)",
         fsn: "screening de otoemisión acústica neonatal (procedimiento)",
-        datosReportables: null
-        // datosReportables: [
-        //     {
-        //         conceptId: '371580005',
-        //         term: 'evaluación de antecedentes',
-        //         valor: {
-        //             conceptId: '160245001',
-        //             nombre: 'sin problemas o incapacidad actual'
-        //         }
-        //     },
-        //     {
-        //         conceptId: '2111000013109',
-        //         term: 'otoemisión acústica de oído izquierdo',
-        //         valor: {
-        //             conceptId: '2261000013100',
-        //             nombre: 'otoemision acustica ausente'
-        //         }
-        //     },
-        //     {
-        //         conceptId: '2101000013106',
-        //         term: 'otoemisión acústica de oído derecho',
-        //         valor: {
-        //             conceptId: '2271000013107',
-        //             nombre: 'otoemisión acustica presente'
-        //         }
-        //     }
-        // ],
+        //datosReportables: null
+        datosReportables: [
+            {
+                conceptId: '371580005',
+                term: 'evaluación de antecedentes',
+                valor: {
+                    conceptId: '160245001',
+                    nombre: 'sin problemas o incapacidad actual'
+                }
+            },
+            {
+                conceptId: '2111000013109',
+                term: 'otoemisión acústica de oído izquierdo',
+                valor: {
+                    conceptId: '2261000013100',
+                    nombre: 'otoemision acustica ausente'
+                }
+            },
+            {
+                conceptId: '2101000013106',
+                term: 'otoemisión acústica de oído derecho',
+                valor: {
+                    conceptId: '2271000013107',
+                    nombre: 'otoemisión acustica presente'
+                }
+            }
+        ],
     },
     organizacion: {
         nombre: "HOSPITAL PROVINCIAL NEUQUEN - DR. EDUARDO CASTRO RENDON",
         cuie: "Q06391",
         idSips: 205
     },
-    // obraSocial: null,
+    obraSocial: null,
     /* Usar este para los que tienen OS*/
-    obraSocial: {
-        codigoFinanciador: '921001',
-        financiador: 'O.S.P. NEUQUEN'
-    },
+    // obraSocial: {
+    //     codigoFinanciador: '921001',
+    //     financiador: 'O.S.P. NEUQUEN'
+    // },
     profesional: {
         nombre: "MARIA LAURA",
         apellido: "MONTEVERDE",
